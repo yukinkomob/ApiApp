ce94c227f61e0cb1c2c23151a5e63a01bc33f359
Nishimura <yukinkopen@gmail.com>
2021-10-26 13:36:17 +0900

    [add] API取得結果を表示するためのアダプタを追加

---
+++ app/src/main/java/jp/techacademy/yuki/nishimura/apiapp/ApiAdapter.kt
@@ -0,0 +1,60 @@
+package jp.techacademy.yuki.nishimura.apiapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class ApiAdapter(private val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val items = mutableListOf<Shop>()
+
+    fun refresh(list: List<Shop>) {
+        items.apply {
+            clear()
+            addAll(list)
+        }
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ApiItemViewHolder(LayoutInflater.from(context).inflate(R.layout.recycler_favorite, parent, false))
+    }
+
+    class ApiItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val rootView: ConstraintLayout = view.findViewById(R.id.rootView)
+        val nameTextView: TextView = view.findViewById(R.id.nameTextView)
+        val imageView: ImageView = view.findViewById(R.id.imageView)
+        val favoriteImageView: ImageView = view.findViewById(R.id.favoriteImageView)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ApiItemViewHolder) {
+            updateAllItemViewHolder(holder, position)
+        }
+    }
+
+    private fun updateAllItemViewHolder(holder: ApiItemViewHolder, position: Int) {
+        val data = items[position]
+        holder.apply {
+            rootView.apply {
+                setBackgroundColor(
+                    ContextCompat.getColor(context,
+                        if (position % 2 == 0) android.R.color.white else android.R.color.darker_gray))
+            }
+            nameTextView.text = data.name
+            Picasso.get().load(data.logoImage).into(imageView)
+            favoriteImageView.setImageResource(R.drawable.ic_star_border)
+        }
+    }
+}
