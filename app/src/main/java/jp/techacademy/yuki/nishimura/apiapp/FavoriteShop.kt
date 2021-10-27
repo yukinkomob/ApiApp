5bde7843bf19059c3848e82bd17fcba3778f8ad4
Nishimura <yukinkopen@gmail.com>
2021-10-27 10:39:09 +0900

    [add] アプリケーションを拡張、Realmを実装、お気に入りデータのCRUDを追加

---
+++ app/src/main/java/jp/techacademy/yuki/nishimura/apiapp/FavoriteShop.kt
@@ -0,0 +1,48 @@
+package jp.techacademy.yuki.nishimura.apiapp
+
+import io.realm.Realm
+import io.realm.annotations.PrimaryKey
+
+open class FavoriteShop {
+    @PrimaryKey
+    var id: String = ""
+    var imageUrl: String = ""
+    var name: String = ""
+    var url: String = ""
+
+    companion object {
+        fun findAll(): List<FavoriteShop> =
+            Realm.getDefaultInstance().use { realm ->
+                realm.where(FavoriteShop::class.java)
+                    .findAll().let {
+                        realm.copyFromRealm(it)
+                    }
+            }
+
+        fun findBy(id: String): FavoriteShop? =
+            Realm.getDefaultInstance().use { realm ->
+                realm.where(FavoriteShop::class.java)
+                    .equalTo(FavoriteShop::id.name, id)
+                    .findFirst()?.let {
+                        realm.copyFromRealm(it)
+                    }
+            }
+
+        fun insert(favoriteShop: FavoriteShop) =
+            Realm.getDefaultInstance().executeTransaction {
+                it.insertOrUpdate(favoriteShop)
+            }
+
+        fun delete(id: String) =
+            Realm.getDefaultInstance().use { realm ->
+                realm.where(FavoriteShop::class.java)
+                    .equalTo(FavoriteShop::id.name, id)
+                    .findFirst()?.also { deleteShop ->
+                        realm.executeTransaction {
+                            deleteShop.deleteFromRealm()
+                        }
+                    }
+            }
+    }
+
+}
