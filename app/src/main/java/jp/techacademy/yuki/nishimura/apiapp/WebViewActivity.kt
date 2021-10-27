23f416654113472d05e1146a09ab0076af76a451
Nishimura <yukinkopen@gmail.com>
2021-10-27 21:24:34 +0900

    [mod] WebViewでのコンテンツ表示を可能にした

--- app/src/main/java/jp/techacademy/yuki/nishimura/apiapp/WebViewActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/nishimura/apiapp/WebViewActivity.kt
@@ -4,12 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_web_view.*
 
 class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+        webView.webViewClient = WebViewClient()
         webView.loadUrl(intent.getStringExtra(KEY_URL).toString())
     }
 
