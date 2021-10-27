30c4363a02cf81969c475ed55b855f87dadb0153
Nishimura <yukinkopen@gmail.com>
2021-10-27 17:57:40 +0900

    [mod] コールバック処理を実装

--- app/src/main/java/jp/techacademy/yuki/nishimura/apiapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/nishimura/apiapp/MainActivity.kt
@@ -1,12 +1,13 @@
 package jp.techacademy.yuki.nishimura.apiapp
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentCallback {
 
     private val viewPagerAdapter by lazy { ViewPagerAdapter(this) }
 
@@ -23,5 +24,42 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
             tab.setText(viewPagerAdapter.titleIds[position])
         }.attach()
+    }
+
+    override fun onAddFavorite(shop: Shop) {
+        FavoriteShop.insert(FavoriteShop().apply {
+            id = shop.id
+            name = shop.name
+            imageUrl = shop.logoImage
+            url = if (shop.couponUrls.sp.isNotEmpty()) shop.couponUrls.sp else shop.couponUrls.pc
+        })
+        (viewPagerAdapter.fragments[VIEW_PAGER_POSITION_FAVORITE] as FavoriteFragment).updateData()
+    }
+
+    override fun onDeleteFavorite(id: String) {
+        showConfirmDeleteFavoriteDialog(id)
+    }
+
+    private fun showConfirmDeleteFavoriteDialog(id: String) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.delete_favorite_dialog_title)
+            .setMessage(R.string.delete_favorite_dialog_message)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                deleteFavorite(id)
+            }
+            .setNegativeButton(android.R.string.cancel) { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun deleteFavorite(id: String) {
+        FavoriteShop.delete(id)
+        (viewPagerAdapter.fragments[VIEW_PAGER_POSITION_API] as ApiFragment).updateView()
+        (viewPagerAdapter.fragments[VIEW_PAGER_POSITION_FAVORITE] as FavoriteFragment).updateData()
+    }
+
+    companion object {
+        private const val VIEW_PAGER_POSITION_API = 0
+        private const val VIEW_PAGER_POSITION_FAVORITE = 1
     }
 }
